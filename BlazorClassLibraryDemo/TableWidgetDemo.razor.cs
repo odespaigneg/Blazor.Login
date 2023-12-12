96c466f776ea4da7f43bd4349709329250cad534
odespaigneg <oglysdesp@gmail.com>
2023-12-11 23:59:29 -0400

    library test
    library test

---
+++ BlazorClassLibraryDemo/TableWidgetDemo.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace Blazor.Login.Library.Demo
+{
+    public partial class TableWidgetDemo<TItem>
+    {
+        [Parameter]
+        public RenderFragment HeaderTemplateDemo { get; set; }
+
+        [Parameter]
+        public RenderFragment<TItem> RowTemplateDemo { get; set; }
+
+        [Parameter]
+        public RenderFragment FooterTemplateDemo { get; set; }
+
+        [Parameter]
+        public IReadOnlyList<TItem> Items { get; set; }
+    }
+}
